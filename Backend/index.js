756475177c8c119b7d78ca8278420a080b2a7d37
tushramishra2304@gmail.com <tusharmishra2304@gmail.com>
2023-04-30 23:52:33 +0530

    Updated index.js

--- Backend/index.js
+++ Backend/index.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const port = 3000;
+require('./Connections/Connection')
+
 
 app.get("/",(req,res)=>{
     res.send("Hello")
 })
 
-app.listen(3000,()=>{
+app.listen(port,()=>{
     console.log("Server is running")
 })
