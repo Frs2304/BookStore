2d13997595ec0bdf45adfc8e2d62eb0b004cb35d
Tushar Mishra <tusharmishra2304@gmail.com>
2023-05-03 00:49:09 +0530

    Update Connection.js

--- Backend/Connections/Connection.js
+++ Backend/Connections/Connection.js
@@ -1,4 +1,4 @@
 const mongoose = require('mongoose');
-mongoose.connect('mongodb+srv://frs23:<Yash2304>@firstcluster.ign9lps.mongodb.net/BookStore?retryWrites=true&w=majority'
+mongoose.connect('mongodb+srv://frs23:<>@firstcluster.ign9lps.mongodb.net/BookStore?retryWrites=true&w=majority'
 ).then(()=> 
 console.log("Connected"))
