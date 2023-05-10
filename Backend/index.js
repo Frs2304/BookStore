36781f6cae4a677d5e9c3b9176111cc12eba4909
tushramishra2304@gmail.com <tusharmishra2304@gmail.com>
2023-05-10 23:53:05 +0530

    Updated index.js

--- Backend/index.js
+++ Backend/index.js
@@ -1,12 +1,11 @@
 const express = require('express');
 const app = express();
-const path = require('path');
 const port = 3000;
+const bookRoute = require("./Routes/Routes");
 require('./Connections/Connection')
 
-
-app.get("/",(req,res)=>{
-    res.send("Hello")
+app.get("/", (req, res)=> {
+    res.send("Hello");
 })
 
 app.listen(port,()=>{
