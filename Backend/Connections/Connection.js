94ffb7fe0fddb220677818123d8cb326f97a3cd7
Tushar Mishra <tusharmishra2304@gmail.com>
2023-06-22 19:28:30 +0530

    Delete Backend directory

--- Backend/Connections/Connection.js
+++
@@ -1,5 +0,0 @@
-const mongoose = require('mongoose');
-
-mongoose.connect('mongodb+srv://frs23:<>@firstcluster.ign9lps.mongodb.net/BookStore?retryWrites=true&w=majority'
-).then(()=> 
-console.log("Connected"));
